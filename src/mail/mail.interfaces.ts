0cccf3ff400f6ad9d2855a587bf9de5067210508
Aleksey Lovchikov <a.lovchikov@opfm.ru>
2021-12-21 18:03:24 +0300

    modify mail module

--- src/mail/mail.interfaces.ts
+++ src/mail/mail.interfaces.ts
@@ -3,3 +3,8 @@ export interface MailModuleOptions {
   domain: string;
   fromEmail: string;
 }
+
+export interface EmailVariables {
+  key: string;
+  value: string;
+}
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,25 +2,29 @@ import { Inject, Injectable } from '@nestjs/common';
 import got from 'got';
 import * as FormData from 'form-data';
 import { CONFIG_OPTIONS } from '../common/common.constants';
-import { MailModuleOptions } from './mail.interfaces';
+import { EmailVariables, MailModuleOptions } from './mail.interfaces';
 
 @Injectable()
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    this.sendEmail('Hello', 'World');
-  }
+  ) {}
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(
+    subject: string,
+    template: string,
+    emailVariables: EmailVariables[],
+  ) {
     const form = new FormData();
     form.append('from', `Excited User <mailgun@${this.options.domain}>`);
     form.append('to', this.options.fromEmail);
     form.append('to', this.options.fromEmail);
     form.append('subject', subject);
-    form.append('template', 'email-verification');
-    form.append('v:username', 'aleksey.lovchikov');
-    form.append('v:code', '1sd8r02klwa');
+    form.append('template', template);
+
+    emailVariables.forEach(variable => {
+      form.append(variable.key, variable.value);
+    });
 
     const res = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
